b2701da92f5636036884c47db46f71b84a2fe74c
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2023-06-26 00:29:53 -0600

    Add borders

---
+++ pincerhex_gui/src/app.rs
@@ -0,0 +1,177 @@
+use eframe::egui;
+use egui::Pos2;
+
+use crate::board::{hex_border, hexagon, BoardCell};
+
+pub struct PincerhexApp {
+    player_is_white: bool,
+    new_game: bool,
+    move_count: u16,
+}
+
+impl Default for PincerhexApp {
+    fn default() -> Self {
+        Self {
+            player_is_white: true,
+            new_game: true,
+            move_count: 0,
+        }
+    }
+}
+
+impl eframe::App for PincerhexApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if self.new_game {
+                self.start_menu(ui);
+            } else {
+                self.hex_board(ctx, ui);
+            }
+        });
+    }
+}
+
+impl PincerhexApp {
+    fn start_menu(&mut self, ui: &mut egui::Ui) {
+        ui.vertical_centered_justified(|ui| {
+            ui.heading("New Game");
+            let label = if self.player_is_white {
+                "White"
+            } else {
+                "Black"
+            };
+            ui.add(egui::Checkbox::new(
+                &mut self.player_is_white,
+                format!("Player colour: {label}"),
+            ));
+            if ui.add(egui::Button::new("Start game")).clicked() {
+                self.new_game = false;
+            }
+        });
+    }
+
+    fn restart(&mut self) {
+        let new_app = Self::default();
+        self.new_game = new_app.new_game;
+        self.player_is_white = new_app.player_is_white;
+    }
+
+    fn hex_board(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
+        ui.vertical(|ui| {
+            ui.heading("Pincerhex");
+            if ui.button("New Game").clicked() {
+                self.restart()
+            }
+            match (self.move_count, self.player_is_white) {
+                (0, true) => {
+                    ui.label("Place a piece anywhere to start.");
+                }
+                (0, false) => {
+                    // TODO: Make a move here
+                }
+                (_, _) => {
+                    ui.label("Your turn.");
+                }
+            }
+        });
+        let rect_size = ctx.screen_rect().size();
+        let dimensions = Dimensions::from_height(rect_size.y);
+        let padding = dimensions.hex_size * 0.75;
+        let start = Pos2::new(rect_size.x / 2., padding);
+        self.cells(start, &dimensions, ui);
+        self.edges(start, &dimensions, ui);
+    }
+
+    fn cells(&mut self, start: Pos2, dimensions: &Dimensions, ui: &mut egui::Ui) {
+        (0..dimensions.board_size).for_each(|y| {
+            let col_start = start.left_down(dimensions.hex_radius(), y);
+            (0..dimensions.board_size).for_each(|x| {
+                hexagon(
+                    ui,
+                    dimensions.hex_size,
+                    col_start.right_down(dimensions.hex_radius(), x),
+                    BoardCell {
+                        piece: None,
+                        idx: (x, y),
+                    },
+                );
+            })
+        });
+    }
+
+    fn edges(&mut self, start: Pos2, dimensions: &Dimensions, ui: &mut egui::Ui) {
+        (0..dimensions.board_size).for_each(|y| {
+            let col_start = start.left_down(dimensions.hex_radius(), y);
+            (0..dimensions.board_size).for_each(|x| {
+                hex_border(
+                    ui,
+                    dimensions.hex_size,
+                    col_start.right_down(dimensions.hex_radius(), x),
+                    dimensions.board_size,
+                    BoardCell {
+                        piece: None,
+                        idx: (x, y),
+                    },
+                );
+            })
+        })
+    }
+}
+
+// {{{ Dimensions
+#[derive(Debug)]
+struct Dimensions {
+    hex_size: f32,
+    board_size: i16,
+}
+
+impl Default for Dimensions {
+    fn default() -> Self {
+        Self {
+            hex_size: 40.,
+            board_size: 11,
+        }
+    }
+}
+
+impl Dimensions {
+    // TODO: Make board horizontal on desktop
+    fn from_height(y: f32) -> Self {
+        let mut dim = Self::default();
+        dim.hex_size = (2. * y) / (3. * dim.board_size as f32);
+        dim
+    }
+
+    fn hex_radius(&self) -> f32 {
+        self.hex_size / 2.
+    }
+}
+// }}}
+
+// {{{ Hex
+const SQRT_3: f32 = 1.7320508075688772;
+const PI_3: f32 = std::f32::consts::PI / 3.;
+
+pub trait Hex {
+    fn right_down(self, size: f32, mul: i16) -> Self;
+    fn left_down(self, size: f32, mul: i16) -> Self;
+}
+
+impl Hex for Pos2 {
+    fn right_down(self, size: f32, mul: i16) -> Self {
+        let len = size * SQRT_3;
+        let x = self.x + len * PI_3.cos() * mul as f32;
+        let y = self.y + len * PI_3.sin() * mul as f32;
+        Self::new(x, y)
+    }
+
+    fn left_down(self, size: f32, mul: i16) -> Self {
+        let len = size * SQRT_3;
+        let x = self.x - len * PI_3.cos() * mul as f32;
+        let y = self.y + len * PI_3.sin() * mul as f32;
+        Self::new(x, y)
+    }
+}
+// }}}
+
+// vim:foldmethod=marker
---
+++ pincerhex_gui/src/board.rs
@@ -0,0 +1,103 @@
+use std::iter;
+
+use eframe::{egui, epaint};
+use egui::{Color32, Pos2, Stroke};
+
+pub struct BoardCell {
+    pub piece: Option<Piece>,
+    pub idx: (i16, i16),
+}
+
+pub enum Piece {
+    Black,
+    White,
+}
+
+fn hex_points(center: Pos2, radius: f32) -> Vec<Pos2> {
+    (0..6)
+        .map(|i| {
+            let angle = (std::f32::consts::PI / 3. * (i as f32)) + (std::f32::consts::PI / 6.);
+            let x = center.x + radius * angle.cos();
+            let y = center.y + radius * angle.sin();
+            Pos2::new(x, y)
+        })
+        .collect()
+}
+
+pub fn hex_border(ui: &mut egui::Ui, size: f32, center: Pos2, board_size: i16, cell: BoardCell) {
+    let radius = size / 2.;
+    let points = hex_points(center, radius);
+
+    // TODO: split the vertical segments in half to create the "split" border
+    // Not sure if it's worth our time to do that right now though
+    points
+        .iter()
+        // Need another one to close the loop
+        .chain(iter::once(points.first().unwrap()))
+        .collect::<Vec<&Pos2>>()
+        .windows(2)
+        .enumerate()
+        .for_each(|(idx, pair)| {
+            if let [a, b] = pair {
+                let (width, colour) = match (idx, cell.idx) {
+                    (2 | 3, (0, _)) => (4., Color32::WHITE),
+                    (4 | 5, (_, 0)) => (4., Color32::BLACK),
+                    (0 | 5, (x, _)) if x == board_size - 1 => (4., Color32::WHITE),
+                    (1 | 2, (_, y)) if y == board_size - 1 => (4., Color32::BLACK),
+                    _ => (0., Color32::TRANSPARENT),
+                };
+                ui.painter()
+                    .line_segment([**a, **b], Stroke::new(width, colour));
+            }
+        });
+}
+
+pub fn hexagon(ui: &mut egui::Ui, size: f32, center: Pos2, cell: BoardCell) {
+    let radius = size / 2.;
+    let points = hex_points(center, radius);
+
+    ui.painter().add(epaint::Mesh {
+        indices: vec![0, 1, 2, 3, 4, 5, 0, 2, 3, 0, 5, 3],
+        vertices: points
+            .iter()
+            .map(|&pos| epaint::Vertex {
+                pos,
+                uv: Default::default(),
+                color: Color32::KHAKI,
+            })
+            .collect(),
+        ..Default::default()
+    });
+
+    points
+        .iter()
+        // Need another one to close the loop
+        .chain(iter::once(points.first().unwrap()))
+        .collect::<Vec<&Pos2>>()
+        .windows(2)
+        .for_each(|pair| {
+            if let [a, b] = pair {
+                ui.painter()
+                    .line_segment([**a, **b], Stroke::new(1., Color32::DARK_GRAY));
+            }
+        });
+
+    if let Some(colour) = cell.piece {
+        ui.painter().circle_filled(
+            center,
+            radius / 1.5,
+            match colour {
+                Piece::Black => Color32::BLACK,
+                Piece::White => Color32::WHITE,
+            },
+        );
+    } else {
+        ui.painter().text(
+            center,
+            egui::Align2::CENTER_CENTER,
+            format!("{}{}", (cell.idx.0 + 97) as u8 as char, cell.idx.1 + 1),
+            epaint::FontId::default(),
+            Color32::BLACK,
+        );
+    }
+}
