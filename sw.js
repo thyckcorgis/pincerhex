7bd0b5a0df49edc868df628d48dcef3915409bc4
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2023-06-29 20:43:51 -0600

    Deploy website - based on 780cfb423bee1a4d22674394c34205f68c8cda4a

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'pincerhex-pwa';
+const filesToCache = [
+  './',
+  './index.html',
+  './pincerhex_gui.js',
+  './pincerhex_gui_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
